6aca9f169529753b2eaaa35111484060b666d4ce
KarolKotomski <karol.kotomski88@gmail.com>
2023-12-06 17:56:08 +0100

    tailwind basic setup, tailwind prettier installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "2rem",
+      },
+    },
+
+    extend: {
+      colors: {
+        orange: "#ff4d30",
+      },
+
+      backgroundImage: {},
+    },
   },
   plugins: [],
-}
-
+};
