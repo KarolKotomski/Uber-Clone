854f3a82147ebce088324a05793a5e346108e86f
KarolKotomski <karol.kotomski88@gmail.com>
2023-12-11 15:42:37 +0100

    HomeHero jumping bottom bar effect applied

--- src/components/HomeHeroElement.tsx
+++ src/components/HomeHeroElement.tsx
@@ -4,21 +4,39 @@ import { heroList } from "../data/data";
 import { useState } from "react";
 
 type Props = {
-  handleChangeBackgroundImage: (id:number) => void
-}
+  handleChangeBackgroundImage: (id: number) => void;
+};
 
-const HomeHeroElement = ({handleChangeBackgroundImage}:Props) => {
+const HomeHeroElement = ({ handleChangeBackgroundImage }: Props) => {
   const [activeHeaderId, setActiveHeaderId] = useState(1);
+  const [translationDistance, setTranslationDistance] = useState("0.975px");
 
   const mainContent = heroList.find((element) => element.id === activeHeaderId);
 
   const handleClick = (id: number) => {
     setActiveHeaderId(id);
     handleChangeBackgroundImage(id);
+    handleTranslationDistance(id);
+  };
+
+  const handleTranslationDistance = (id: number) => {
+    switch (id) {
+      case 1:
+        setTranslationDistance("0.975px");
+        break;
+      case 2:
+        setTranslationDistance("3.1px");
+        break;
+      case 3:
+        setTranslationDistance("5.225px");
+        break;
+      default:
+        setTranslationDistance("0.975px");
+    }
   };
   return (
     <div className="flex flex-col bg-white text-black">
-      <div className="flex border-b border-b-[rgb(238,238,238)] xl:justify-evenly">
+      <div className="relative flex border-b border-b-[rgb(238,238,238)] xl:justify-evenly">
         {heroList.map((element) => {
           return (
             <HomeHeroElementHeader
@@ -30,7 +48,15 @@ const HomeHeroElement = ({handleChangeBackgroundImage}:Props) => {
             />
           );
         })}
+
+        <div
+          style={{
+            transform: `scaleX(80) translateX(${translationDistance})`,
+          }}
+          className="absolute bottom-0 left-0 h-1 w-[0.0625rem] origin-left bg-black transition-transform duration-[600ms] ease-[cubic-bezier(0.19,1,0.22,1)]"
+        ></div>
       </div>
+
       <HomeHeroElementMain
         header={mainContent?.header}
         paragraph={mainContent?.paragraph}
