016f67861c908f9a64420f601b43ed3b963f99fa
KarolKotomski <karol.kotomski88@gmail.com>
2024-01-08 15:52:40 +0100

    map initial zoom bug fixed.

--- src/components/GoogleMapSection.tsx
+++ src/components/GoogleMapSection.tsx
@@ -16,9 +16,11 @@ const GoogleMapSection = () => {
   const [map, setMap] = useState(null);
 
   const onLoad = useCallback(function callback(map: any) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    // const bounds = new window.google.maps.LatLngBounds(center);
+    // // map.fitBounds(bounds);
 
+    const zoom = 13;
+    map.setZoom(zoom);
     setMap(map);
   }, []);
 
@@ -30,7 +32,7 @@ const GoogleMapSection = () => {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={5}
+      zoom={14}
       onLoad={onLoad}
       onUnmount={onUnmount}
       options={{
