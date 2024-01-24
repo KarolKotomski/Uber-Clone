71da8b35a4b751f9690637b23608dd503c9375b2
KarolKotomski <karol.kotomski88@gmail.com>
2024-01-24 14:26:13 +0100

    lazy loading added to App. Fallback component created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Ride from "./pages/Ride";
-import NoMatch from "./pages/NoMatch";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { LoadScript } from "@react-google-maps/api";
 import { Library } from "@googlemaps/js-api-loader";
+import { Suspense, lazy } from "react";
+import FallBack from "./components/FallBack";
 
 const libraries: Library[] = ["places"];
 
 function App() {
   const apiKey = process.env.REACT_APP_API_KEY || "";
 
+  const Home = lazy(() => import("./pages/Home"));
+  const Ride = lazy(() => import("./pages/Ride"));
+  const NoMatch = lazy(() => import("./pages/NoMatch"));
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <LoadScript googleMapsApiKey={apiKey} libraries={libraries}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="ride" element={<Ride />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
+          <Suspense fallback={<FallBack />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="ride" element={<Ride />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </Suspense>
         </LoadScript>
       </BrowserRouter>
     </Provider>
