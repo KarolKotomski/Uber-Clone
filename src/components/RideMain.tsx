267fe11338e0302979f6673904d4d26d8768f042
KarolKotomski <karol.kotomski88@gmail.com>
2024-01-31 13:11:23 +0100

    scroll bug on active search menu attempt1.

--- src/components/RideMain.tsx
+++ src/components/RideMain.tsx
@@ -19,9 +19,13 @@ const RideMain = () => {
   useEffect(() => {
     if (isSearchMenuActive === true) {
       document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
+      document.body.style.width = "100%";
       window.scrollTo(0, 0);
     } else {
       document.body.style.overflow = "auto";
+      document.body.style.position = "static";
+      document.body.style.width = "auto";
     }
   }, [isSearchMenuActive]);
 
